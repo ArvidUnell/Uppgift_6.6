5486018a31db5958a1111a2e89ef0deedce745a8
ArvidUnell <arun06001@utb.vaxjo.se>
2024-02-14 13:04:23 +0100

    Add project files.

---
+++ Uppgift_6.6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Uppgift_6_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VarannanLiten("Detta är ett exempel. Texten ändras när den skrivs ut.");
+        }
+        static void VarannanLiten(string meddelande)
+        {
+            string[] meddelandeArr = meddelande.Split(' ');
+
+            for (int i = 0;i < meddelandeArr.Length;i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(meddelandeArr[i].ToUpper());
+                }
+                else
+                {
+                    Console.Write(meddelandeArr[i].ToLower());
+                }
+                Console.Write(' ');
+            }
+        }
+    }
+}
